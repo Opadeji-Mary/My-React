54d750d029c83d592f2927f31c7f2f39d840075e
[opadeji mary] <feranmiopadeji@gmail.com>
2024-02-13 14:43:09 +0100

    classwork

--- src/components/Apibackend.jsx
+++ src/components/Apibackend.jsx
@@ -21,12 +21,12 @@ const Apibackend = (req, res) => {
         data.map((data)=>(
           <div key={data.id}>
             <div className="flex items-center text-left gap-40 px-7">
-              <h1 className="w-1/2">{data.name}</h1>
-              <h1 className="text-left w-1/2">{data.username}</h1>
+              <h1 className="w-1/2"key={data.firstName}></h1>
+              <h1 className="text-left w-1/2"key={data.lastName}></h1>
             </div>
             <div className="mt-10 flex">
-              <h1>{data.email}</h1>
-              <h1>{data.gender}</h1>
+              <h1 key={data.email}></h1>
+              <h1 key={data.gender}></h1>
             </div>
           </div>
         ))
